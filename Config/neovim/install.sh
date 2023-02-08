ac621daa70ca9549bf14ea9d7df939d378d7f1c3
kiotosi <shilo@firecode.ru>
2023-02-08 10:14:21 +0300

    feat(neovim): add neovim installation through brew

--- Config/neovim/install.sh
+++ Config/neovim/install.sh
@@ -4,3 +4,8 @@
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+# Install Neovim
+xcode-select --install
+brew install --HEAD tree-sitter
+brew install --HEAD luajit
+brew install --HEAD neovim

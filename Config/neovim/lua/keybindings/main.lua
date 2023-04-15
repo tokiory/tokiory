c96a575a6d9f877505743ff2a99ac6ed0ba21529
kiotosi <shilo@firecode.ru>
2023-04-15 12:48:09 +0300

    feat(config): remove old configuration

--- Config/neovim/lua/keybindings/main.lua
+++
@@ -1,7 +0,0 @@
--- Файл для настройки горячих клавиш в Neovim
--- Автор: Даниил Шило <crckhd10@gmail.com>
--- Лицензия: GPL 3.0
-require('keybindings/plugins')
-require('keybindings/utils')
-
-im('<C-k>', '<escape>')

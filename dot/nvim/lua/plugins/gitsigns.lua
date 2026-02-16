7d2f8abe0350a2517c52d0fd0810bf2bf15436c0
Daniil Shilo <tokiory.work@gmail.com>
2026-02-16 04:50:19 +0300

    feat(nvim): changed default key for git signs

--- dot/nvim/lua/plugins/gitsigns.lua
+++ dot/nvim/lua/plugins/gitsigns.lua
@@ -3,7 +3,7 @@ return {
   lazy = false,
   opts = {},
   keys = {
-    {"g'", '<cmd>Gitsigns preview_hunk<cr>', desc = 'Preview the hunk'},
+    {"<leader>'", '<cmd>Gitsigns preview_hunk<cr>', desc = 'Preview the hunk'},
     {"]c", '<cmd>Gitsigns next_hunk<cr>', desc = 'Show next hunk'},
     {"[c", '<cmd>Gitsigns prev_hunk<cr>', desc = 'Show previous hunk'},
   }

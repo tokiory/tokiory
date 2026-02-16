cf0040de6ea596abc1d49b27a1c607257152d870
Daniil Shilo <tokiory.work@gmail.com>
2026-02-16 03:49:03 +0300

    feat: changed setup_czcfg

--- dot/setup_czcfg.sh
+++ dot/setup_czcfg.sh
@@ -4,34 +4,40 @@
 # Computes which shell user is using and creates a czcfg alias for using chezmoi with the current config
 
 
-echo "Current shell is: $SHELL"
-echo "Setting up czcfg alias for using chezmoi with the current config..."
+CURRENT_DIRECTORY=$(pwd)
+CONFIG_FILE=""
 
-# Add check if alias already exists in the shell configuration file
+echo "Current shell is: $SHELL"
+echo "Current directory is: $CURRENT_DIRECTORY"
+echo "Setting up czcfg alias for using chezmoi with the current config..."
 
 case "$SHELL" in
     *zsh*)
-        if grep -q "alias czcfg='chezmoi --source ~/.config/chezmoi'" ~/.zshrc; then
+        if grep -q "alias czcfg='chezmoi --source $CURRENT_DIRECTORY'" ~/.zshrc; then
           echo "Alias czcfg already exists in ~/.zshrc"
           exit 0
         fi
+        CONFIG_FILE=~/.zshrc
         ;;
     *bash*)
-        if grep -q "alias czcfg='chezmoi --source ~/.config/chezmoi'" ~/.bashrc; then
+        if grep -q "alias czcfg='chezmoi --source $CURRENT_DIRECTORY'" ~/.bashrc; then
           echo "Alias czcfg already exists in ~/.bashrc"
           exit 0
         fi 
+        CONFIG_FILE=~/.bashrc
         ;;
     *fish*)
-        if grep -q "alias czcfg='chezmoi --source ~/.config/chezmoi'" ~/.config/fish/config.fish; then
+        if grep -q "alias czcfg='chezmoi --source $CURRENT_DIRECTORY'" ~/.config/fish/config.fish; then
           echo "Alias czcfg already exists in ~/.config/fish/config.fish"
           exit 0
         fi
+        CONFIG_FILE=~/.config/fish/config.fish
         ;;
     *)
         echo "Unsupported shell. Please add the following alias to your shell configuration file:"
-        echo "alias czcfg='chezmoi --source ~/.config/chezmoi'"
+        echo "alias czcfg='chezmoi --source $CURRENT_DIRECTORY'"
         ;;
     esac
 
+echo "alias czcfg='chezmoi --source $CURRENT_DIRECTORY'" >> $CONFIG_FILE
 echo "Alias czcfg has been set up. Please restart your terminal or source your shell configuration file to use it."

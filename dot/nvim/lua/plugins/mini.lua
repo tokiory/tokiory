7bedab1d3dc8cebb931fcc87d77bbe603da235fd
Daniil Shilo <tokiory.work@gmail.com>
2026-02-18 02:28:48 +0300

    feat(neovim): add mini.pairs plugin

--- dot/nvim/lua/plugins/mini.lua
+++ dot/nvim/lua/plugins/mini.lua
@@ -6,6 +6,7 @@ return {
     -- { '<leader>v', function() require('mini.files').open() end, desc = 'Open file explorer' },
   },
   config = function()
+    require('mini.pairs').setup()
     -- require('mini.diff').setup()
     -- require('mini.icons').setup()
     -- require('mini.files').setup()
